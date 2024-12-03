304b3500b80a88ccf2b3162e57ae44ebaa5a4a8d
eli <elijahmurket@gmail.com>
2024-12-03 13:47:38 +0300

    fixed spacing

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -36,7 +36,7 @@ const HeroSection = () => {
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
            I love developing software
           </p>
-          <div style={{ marginBottom: '2.5 rem'}}>
+          <div style={{ marginBottom: '2.5rem'}}>
             <Link
               href="/#contact"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
